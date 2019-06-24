0fe28149f4e341009b22979c381369e04f85c3b7
jball <jeffpball@gmail.com>
2019-06-23 21:05:35 -0700

    Add all major functionality except modal

---
+++ FriendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,73 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on each friend and their survey results
+// ===============================================================================
+
+let friendsAPI = require("../data/friendsData")
+
+// Routing 
+module.exports = function(app) {
+    //grabs data friends array from the friendsData.js file
+    app.get("/api/friends", function(req, res) {
+        
+        // Changed this to 
+        res.json(friendsAPI);
+    });
+    
+    // A POST route for /api/freinds that will handle incoming survey results and house compatability logic
+    // Create a new friend - takes in JSON input
+    
+    
+    app.post("/api/friends", function(req, res) { 
+        // this pushes user survey data to freindsData 
+        friendsAPI.push(req.body);
+        
+        // console.log(req.body)
+        
+        // First user 
+        //console.log(friendsAPI[0]);
+        //console.log(friendsAPI)
+        
+        
+        
+        // Declare y which hold the sum of the diffrences of the array of answers between 2 users 
+        let y = 0;
+        
+        // An empty array to hold score comparisons
+        let comparisonArray = [];
+        
+        for (var j = 1; j < friendsAPI.length; j ++) {
+
+            // Empty out y to start from zero
+            y = 0 
+
+            for (var i = 0; i < 4; i ++) { // 4 will turn to ten 
+                let x = Math.abs(friendsAPI[0].scores[i] - friendsAPI[j].scores[i])
+                y += x
+                
+                console.log(x)
+            };
+            comparisonArray.push(y)
+            console.log(y);
+            console.log(comparisonArray)
+            // Holds the minimum of the comparisons of the score arrays for each user
+            let minComparison = Math.min(...comparisonArray)
+            //
+            console.log(minComparison) 
+            
+            // Gives the index of the match we should grab from our friendsAPI object
+            let matchIndex = comparisonArray.indexOf(minComparison)
+            
+            console.log(matchIndex)
+
+            // final match 
+            let finalMatch = friendsAPI[matchIndex +1];
+
+            console.log(finalMatch)
+
+            
+        }
+     
+    });
+};
---
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -0,0 +1,34 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+
+// GET Route to `/survey` which should display the survey page.
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+
+// Default, catch-all route that leads to `home.html` which displays the home page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
